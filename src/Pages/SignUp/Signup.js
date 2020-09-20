c567fd9fea341d05f16cb9374a0b03db9f5ec29e
kuntalghosh <kuntalghosh.ewu@gmail.com>
2020-09-20 17:19:53 +0600

    signup validation added

--- src/Pages/SignUp/Signup.js
+++ src/Pages/SignUp/Signup.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Checkbox from "@material-ui/core/Checkbox";
 import navbarColorContext from "../../Context/NavbarColorContext";
 import styles from "./Signup.module.scss";
@@ -20,7 +20,8 @@ import {
 import DateFnsUtils from "@date-io/date-fns";
 import Grid from "@material-ui/core/Grid";
 import cx from "classname";
-
+import SocialLoginButton from "../../Components/Social_Login_Button/Social_Login_Button";
+import * as firebase from "../../services/firebase.auth";
 const useStyles = makeStyles((theme) => ({
   root: {
     // width: "100%",
@@ -57,13 +58,26 @@ const useStyles = makeStyles((theme) => ({
 
 const Signup = () => {
   const classes = useStyles();
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    error: {},
+  });
 
   return (
     <>
       <div className={styles.signin}>
         <div className={styles.signin_form}>
           <div className={cx(styles.booking_form, "d-flex")}>
-            <form className={classes.root} noValidate autoComplete="off">
+            <form
+              onSubmit={handleSubmit}
+              className={classes.root}
+              noValidate
+              autoComplete="off"
+            >
               <div>
                 <h1>Create an account</h1>
               </div>
@@ -72,6 +86,11 @@ const Signup = () => {
                 id="standard-full-width"
                 label="First Name"
                 className={styles.textfield}
+                error={user.error.firstName ? true : false}
+                name="firstName"
+                value={user.firstName}
+                onChange={handleChange}
+                helperText={user.error.firstName}
                 // input:classes.input
                 // classes={{
                 //   input: classes.input,
@@ -92,6 +111,11 @@ const Signup = () => {
                 id="standard-full-width"
                 label="Last Name"
                 className={styles.textfield}
+                error={user.error.lastName ? true : false}
+                name="lastName"
+                value={user.lastName}
+                onChange={handleChange}
+                helperText={user.error.lastName}
                 // input:classes.input
                 // classes={{
                 //   input: classes.input,
@@ -112,6 +136,11 @@ const Signup = () => {
                 id="standard-full-width"
                 label="Username or Email"
                 className={styles.textfield}
+                error={user.error.email ? true : false}
+                name="email"
+                value={user.email}
+                onChange={handleChange}
+                helperText={user.error.email}
                 // input:classes.input
                 // classes={{
                 //   input: classes.input,
@@ -134,6 +163,11 @@ const Signup = () => {
                 type="password"
                 style={{ margin: 8 }}
                 fullWidth
+                error={user.error.password ? true : false}
+                name="password"
+                value={user.password}
+                onChange={handleChange}
+                helperText={user.error.password}
                 // defaultValue={selectedPlace && `${selectedPlace.placeName}`}
                 // value={selectedPlace && `${selectedPlace.placeName}`}
                 // readOnly={false}
@@ -148,6 +182,11 @@ const Signup = () => {
                 type="password"
                 style={{ margin: 8 }}
                 fullWidth
+                error={user.error.confirmPassword ? true : false}
+                name="confirmPassword"
+                value={user.confirmPassword}
+                onChange={handleChange}
+                helperText={user.error.confirmPassword}
                 // defaultValue={selectedPlace && `${selectedPlace.placeName}`}
                 // value={selectedPlace && `${selectedPlace.placeName}`}
                 // readOnly={false}
@@ -156,12 +195,28 @@ const Signup = () => {
                 // }}
               />
 
-              <Button variant="contained" fullWidth>
+              <Button variant="contained" type="submit" fullWidth>
                 Create an account
               </Button>
               <div className="text-center mt-4">
                 Already have an account?
                 <Link to="/signup">Login</Link>
+              </div>
+              <span className="mt-3">
+                <hr />
+              </span>
+
+              <div onClick={facebookSignIN}>
+                <SocialLoginButton
+                  imageUrl="/images/fb.png"
+                  buttonText="Continue with Facebook account"
+                ></SocialLoginButton>
+              </div>
+              <div onClick={googleSignIN}>
+                <SocialLoginButton
+                  imageUrl="/images/google.png"
+                  buttonText="Continue with Google account"
+                ></SocialLoginButton>
               </div>
             </form>
           </div>
@@ -169,6 +224,96 @@ const Signup = () => {
       </div>
     </>
   );
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log("submitted");
+    // console.log(e.target.name);
+    let newUser = { ...user };
+
+    if (newUser.email === "") {
+      newUser.error.firstName = "Firstname is required";
+    } else delete newUser.error.firstName;
+
+    if (newUser.lastName === "") {
+      newUser.error.lastName = "Lastname is required";
+    } else delete newUser.error.lastName;
+
+    if (newUser.email === "") {
+      newUser.error.email = "Email is required";
+    } else delete newUser.error.email;
+
+    if (newUser.password === "") {
+      newUser.error.password = "Password is required";
+    } else delete newUser.error.password;
+
+    if (newUser.confirmPassword === "") {
+      newUser.error.confirmPassword = "Confirm Password is required";
+    } else delete newUser.error.confirmPassword;
+    setUser(newUser);
+
+    let errors = Object.keys(user.error).length > 0;
+
+    if (!errors) {
+      try {
+        let response = await firebase.signinWithEmailPassword(
+          user.email,
+          user.password
+        );
+        console.log("response");
+        console.log(response);
+      } catch (e) {
+        console.log(e.message);
+        const newUser = { ...user };
+        newUser.error.message = e.message;
+        setUser(newUser);
+      }
+    }
+
+    // console.log("submit response");
+    // console.log(response);
+  }
+
+  function handleChange({ target: { name, value } }) {
+    console.log(name);
+    let newUser = { ...user };
+    newUser[name] = value.trim();
+
+    if (name === "email") {
+      if (value.trim() === "") {
+        newUser.error.email = "email is required";
+      } else {
+        newUser.error.email = "";
+      }
+    }
+
+    if (name === "password") {
+      if (value.trim() === "") {
+        newUser.error.password = "password is required";
+      } else {
+        newUser.error.password = "";
+      }
+    }
+
+    setUser(newUser);
+  }
+
+  function facebookSignIN() {}
+
+  async function googleSignIN() {
+    console.log("google clicked");
+    let newUser = { ...user };
+
+    try {
+      const { user } = await firebase.signupwithEmailPassword();
+      console.log(user);
+      newUser.email = user.email;
+      newUser.password = user.password;
+      setUser(newUser);
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
 };
 
 export default Signup;
